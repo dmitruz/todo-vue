53351b8b9fa677e2789522f41df0fd519b65216f
dmitruz <dmitruz970@gmail.com>
2023-02-09 21:24:48 +0200

    marked table

--- TodoList/src/App.vue
+++ TodoList/src/App.vue
@@ -1,10 +1,30 @@
 <template>
-  <div id="app">
-    <h1>My To-Do List</h1>
-    <ul>
-      <li>
-        <to-do-item label="My ToDo Item" :done="true"></to-do-item>
-      </li>
-    </ul>
+  <div class="container">
+    <h2 class="text-center mt-5">My Todo</h2>
+    <input type="text" placeholder="Enter text" class="form-control" />
+    <button class="btn btn-warning rounded-0">Submit</button>
+
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">Last</th>
+          <th scope="col" class="text-center">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>HVYGVYYVFYTYFVTg</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script></script>
+
+<style scoped></style>
