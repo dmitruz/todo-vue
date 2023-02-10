9796a30559efbe415bb661fbf796bd9555d4aa97
dmitruz <dmitruz970@gmail.com>
2023-02-10 20:37:17 +0200

    feature: make loop todo.  tasks

--- TodoList/src/App.vue
+++ TodoList/src/App.vue
@@ -4,27 +4,51 @@
     <input type="text" placeholder="Enter text" class="form-control" />
     <button class="btn btn-warning rounded-0">Submit</button>
 
-    <table class="table table-bordered">
+    <table class="table table-bordered mt-5">
       <thead>
         <tr>
           <th scope="col">Task</th>
           <th scope="col">Status</th>
-          <th scope="col" class="text-center">Last</th>
-          <th scope="col" class="text-center">Handle</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>HVYGVYYVFYTYFVTg</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
+        <tr v-for="(task, index) in tasks" :key="index">
+          <td>Learn Vue.js</td>
+          <td>{{ task.name }}</td>
+          <td>{{ task.status }}</td>
+          <div class="text-center">
+            <span class="fa fa-pen"></span>
+          </div>
+
+          <td>
+            <div class="text-center">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
 
-<script></script>
+<script>
+data(){
+ return {
+   tasks: [
+
+ {
+      name: 'Learn Vue.js',
+      status: 'to-do'
+    },
+ {
+      name: 'Learn React.js',
+      status: 'in-progress'
+    },
+   ]
+ }
+}
+</script>
 
 <style scoped></style>
